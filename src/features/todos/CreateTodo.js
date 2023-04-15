a77d6b54c011b6358496c349f901b1be447ca937
Sereya Tiampati <sereyatiampatistudies@gmail.com>
2023-04-15 16:30:13 +0300

    done

--- src/features/todos/CreateTodo.js
+++ src/features/todos/CreateTodo.js
@@ -1,7 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
+import { useDispatch } from "react-redux";
+import { todoAdded } from "./todosSlice";
 
 function CreateTodo() {
-  return <div>Create Todo Component</div>;
+  const [text, setText]= useState("")
+  const dispatch = useDispatch();
+
+  function handleSumbit(e) {
+    e.preventDefault();
+    dispatch(todoAdded(text))
+    setText("")
+  }
+  return <div>
+     <form onSubmit={handleSumbit}>
+        <p>
+          <label>add todo</label>
+          <input type="text" value={text} onChange={(e)=> setText(e.target.value)} />
+        </p>
+        <input type="submit" />
+      </form>
+      <p>Form Text: {text}</p>
+  </div>;
 }
 
 export default CreateTodo;
